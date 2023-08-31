b26c152943b63527a16e8a9d68dff438fe9a55ba
brown <86018533+2brownc@users.noreply.github.com>
2023-08-31 11:39:02 +0530

    top menu icons

--- components/topMenu.tsx
+++ components/topMenu.tsx
@@ -1,18 +1,34 @@
+import Image from 'next/image';
+
 function TopMenu() {
   return (
     <div className="
         flex items-center w-full
         z-10 p-4
       ">
-      <div className="grow text-center">
+      <div className="grow text-center text-xl">
 
         #Vibe Page
       </div>
-      <div className="grow-0 w-8 text-center">
-        B
+      <div className="grow-0 w-8">
+        <a href="https://web.furrl.in/wishlist">
+          <Image
+            src="/images/wishlist_black.png"
+            alt="shoping cart"
+            height={24}
+            width={24}
+          />
+        </a>
       </div>
-      <div className="grow-0 w-8 text-center">
-        C
+      <div className="grow-0 w-8">
+        <a href="https://web.furrl.in/cart">
+          <Image
+            src="/images/bag_black.png"
+            alt="shoping cart"
+            height={24}
+            width={24}
+          />
+        </a>
       </div>
     </div>
   )
