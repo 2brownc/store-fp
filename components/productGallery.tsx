e265e3bf77bb651c5d861c0ea4a403ec7458cadd
brown <86018533+2brownc@users.noreply.github.com>
2023-08-31 10:27:52 +0530

    product gallery

---
+++ components/productGallery.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { IProduct, ICategory } from '@/types/types';
+import ProductCard from '@/components/productCard';
+
+type TProductGallery = {
+  products: IProduct[],
+  selectedProductsCategory: ICategory,
+  currentTotalProducts: number,
+  page: number,
+  setPage: (value: number) => void
+}
+
+function ProductGallery({
+  products,
+  selectedProductsCategory,
+  currentTotalProducts,
+  page,
+  setPage
+}: TProductGallery) {
+  if (!products) return <></>
+
+  return products.map((product: IProduct, key: number) => <ProductCard
+    product={product}
+    selectedProductsCategory={selectedProductsCategory}
+    isLast={key === currentTotalProducts}
+    newLimit={() => setPage(page + 1)}
+  />)
+}
+
+export default ProductGallery;
